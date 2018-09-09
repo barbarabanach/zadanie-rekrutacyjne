aa102645c9504e999e04e69ada9dbaa4cbd0aea2
Basia <krzewicka.barbara@gmail.com>
2018-09-09 11:34:09 +0200

    fixing the typo in project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'billennium-zadanie-bbbanach'`, async(() => {
+  it(`should have as title 'billennium-zadanie-bbanach'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('billennium-zadanie-bbbanach');
+    expect(app.title).toEqual('billennium-zadanie-bbanach');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to billennium-zadanie-bbbanach!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to billennium-zadanie-bbanach!');
   }));
 });
