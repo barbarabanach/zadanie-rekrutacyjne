0c8a21dc1c130f35788512ad6fb7fd87bb33bea0
Basia <krzewicka.barbara@gmail.com>
2018-09-09 20:44:27 +0200

    cleaned html and scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import { CardSectionComponent } from './card-section/card-section.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidemenuComponent,
-    CardSectionComponent
+    CardSectionComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule
