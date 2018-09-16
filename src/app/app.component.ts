56ea300ca96daf522b89f1b8dd76aca04d8e1cf1
Basia <krzewicka.barbara@gmail.com>
2018-09-16 20:40:15 +0200

    data binding of mock config of cards in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bbanach';
+
+  containers = [
+    {
+      customType: 'videochat',
+      longName: 'Porozmawiaj VIDEO',
+      cardNumber: 30
+    },
+    {
+      customType: 'chat',
+      longName: 'Napisz na CZACIE',
+      cardNumber: 12
+    },
+    {
+      customType: 'cobrowsing',
+      longName: 'Włącz COBROWSING',
+      cardNumber: 20
+    },
+  ];
 }
